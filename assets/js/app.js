0ace3f7a8813110003b8eeac7b551e32d5f0c8cd
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-03-26 12:00:40 +0530

    menulist update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,8 @@ const menuicon = document.querySelector(".menuicon");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("showlist");
 })
+
+
+document.querySelector('.hamburger').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('is-active');
+})
